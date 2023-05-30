44398bd09490bf98eb3b67f4eb03685be9060cce
yewon97 <dpdnjs9936@gmail.com>
2023-05-31 00:17:04 +0900

    header 로고에 링크태그 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BsYoutube } from 'react-icons/bs';
 import Search from './Search';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="flex items-center justify-center h-20 border-b px-5">
-      <h1 className="flex items-center gap-x-3">
-        <BsYoutube className="text-default-red text-3xl" />
-        <span className="text-white text-2xl uppercase font-bold">youtube</span>
+      <h1>
+        <Link href="/" className="flex items-center gap-x-3">
+          <BsYoutube className="text-default-red text-3xl" />
+          <span className="text-white text-2xl uppercase font-bold">
+            youtube
+          </span>
+        </Link>
       </h1>
       <Search />
     </header>
