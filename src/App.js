3011d4e9c740f563449872a72ab990f074e4590e
yewon97 <dpdnjs9936@gmail.com>
2023-05-31 22:07:19 +0900

    리액트 쿼리 사용해서 비동기 상태관리 하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,55 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import VideoCard from '../components/VideoCard';
 import { useParams } from 'react-router-dom';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function Videos() {
-  let { keyword } = useParams();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  const [vidoes, setVidoes] = useState({});
-  const [searchKeyword, setSearchKeyword] = useState(keyword);
-
-  useEffect(() => {
-    setSearchKeyword(keyword);
-  }, [keyword]);
-
-  useEffect(() => {
-    setLoading(true);
-    setError(undefined);
-    fetch(`/videos/search.json?q=${searchKeyword}`)
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], async () => {
+    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
       .then((res) => res.json())
-      .then((data) => {
-        console.log('data: ', data);
-        setVidoes(data);
-        setLoading(false);
-      })
-      .catch((e) => {
-        setError('에러가 발생했음!');
-      })
-      .finally(() => setLoading(false));
-    return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-    };
-  }, [searchKeyword]);
-
-  if (loading) return <p>Loading...</p>;
-
-  if (error) return <p>{error}</p>;
+      .then((data) => data.items);
+  });
 
   return (
     <>
-      <section className='px-4 2xl:px-0'>
-        <ul className="grid gap-4 xl:grid-cols-5 md:grid-cols-3 grid-cols-1">
-          {vidoes.items &&
-            vidoes.items.map((v) => {
-              return (
-                <li key={v.id.videoId}>
-                  <VideoCard video={v} />
-                </li>
-              );
-            })}
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong!!</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
         </ul>
-      </section>
+      )}
     </>
   );
 }
