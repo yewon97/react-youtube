0c385ad34b8909563d43e93bb498765bfaf4965c
yewon97 <dpdnjs9936@gmail.com>
2023-05-31 23:01:35 +0900

    youtube api 통신하기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { search } from '../api/youtube';
 import FakeYoutube from '../api/fakeYoutube';
+import Youtube from '../api/youtube';
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -12,7 +13,7 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(['videos', keyword], () => {
-		const youtube = new FakeYoutube();
+		const youtube = new Youtube();
 		return youtube.search(keyword);
 	});
 
