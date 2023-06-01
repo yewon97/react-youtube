70dae4db1fed0a2a86de0d8e09c97f6863b9c16f
yewon97 <dpdnjs9936@gmail.com>
2023-06-02 00:28:24 +0900

    한글로 시간 출력

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,27 +1,34 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { format, register } from 'timeago.js';
+import koLocale from 'timeago.js/lib/lang/ko'
+
+register('ko', koLocale);
 
 export default function VideoCard({ video, classNm }) {
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+
   return (
-		<Link to={`/videos/watch/${video.id.videoId}`} relative="path" className={classNm}>
-			<div className="aspect-video">
-				<img
-					src={video.snippet.thumbnails.high.url}
-					alt=""
-					className="w-full object-cover rounded-xl"
-				/>
-			</div>
-			<div className='pl-px pr-4'>
-				<h2 className="text-base/[20px] line-clamp-2 mt-4">
-					{video.snippet.title}
-				</h2>
-				<p className="mt-2 text-xs text-stone-400">
-					{video.snippet.channelTitle}
-				</p>
-				<p className="mt-1 text-xs text-stone-400">
-					{video.snippet.publishedAt}
-				</p>
-			</div>
-		</Link>
+    <li>
+      <Link
+        to={`/videos/watch/${video.id.videoId}`}
+        relative="path"
+        className={classNm}
+      >
+        <img
+          src={thumbnails.medium.url}
+          alt={title}
+          className="w-full object-cover rounded-xl"
+        />
+
+        <div className="pl-px pr-4">
+          <h2 className="text-base/[20px] line-clamp-2 mt-4">{title}</h2>
+          <p className="mt-2 text-xs text-stone-400">{channelTitle}</p>
+          <p className="mt-1 text-xs text-stone-400">
+            {format(publishedAt, 'ko')}
+          </p>
+        </div>
+      </Link>
+    </li>
   );
 }
